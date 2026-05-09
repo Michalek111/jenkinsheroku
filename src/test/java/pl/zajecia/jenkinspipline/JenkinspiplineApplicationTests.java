a54fa34900dad5c960c87061ef3943b0b58a11d1
Michał Gwóźdź <Mic.gwozdz@gmail.com>
2026-05-09 14:02:40 +0200

    jenkinsfile

---
+++ src/test/java/pl/zajecia/jenkinspipline/JenkinspiplineApplicationTests.java
@@ -0,0 +1,33 @@
+package pl.zajecia.jenkinspipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class JenkinspiplineApplicationTests {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Jenkinspipline"));
+
+    }
+
+}
